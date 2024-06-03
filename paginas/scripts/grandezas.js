23591652774f2dab5908ece1d53905af32e7c730
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-06-02 23:40:14 -0300

    grandezas eletricas concluidas

--- paginas/scripts/grandezas.js
+++ paginas/scripts/grandezas.js
@@ -1,85 +1,130 @@
-var vCorrenteTx = window.document.getElementById('corrente-v')
-var vResistenciaTx = window.document.getElementById('resistencia-v')
-var vPotenciaTx = window.document.getElementById('potencia-v')
-
-var iTensaoTx = window.document.getElementById('tensao-i')
-var iResistenciaTx = window.document.getElementById('resistencia-i')
-var iPotenciaTx = window.document.getElementById('potencia-i')
-
-var rCorrenteTx = window.document.getElementById('corrente-r')
-var rTensaoTx = window.document.getElementById('tensao-r')
-var rPotenciaTx = window.document.getElementById('potencia-r')
-
-var wTensaoTx = window.document.getElementById('tensao-w')
-var wCorrenteTx = window.document.getElementById('corrente-w')
-var wResistenciaTx = window.document.getElementById('resistencia-w')
-
-var vRes = window.document.getElementById('res-v')
-var iRes = window.document.getElementById('res-i')
-var wRes = window.document.getElementById('res-w')
-var rRes = window.document.getElementById('res-r')
-
-var caixaCorrente = document.getElementById('ampere')
-var caixaTensao = document.getElementById('volt')
 
 
-var calcular = window.document.querySelector('input#calcular')
+function tensao(w, a, o, resultado, calc){
+    let corrente = Number(a.value)
+    let resistencia = Number(o.value)
+    let potencia = Number(w.value)
+    let res = resultado
+    let botao = calc
 
-calcular.addEventListener('mouseenter' , entrou)
-calcular.addEventListener('mouseout' , saiu)
+    botao.style.background = '#F7EE79'
+    botao.style.color = 'black'
+    botao.style.boxShadow = '1px 1px 3px #64656B'
+    botao.style.fontWeight = 'bold'
 
-
-function entrou(){
-    calcular.style.background = '#F5E102'
-    calcular.style.color = '#0C1E34'
-    calcular.style.fontWeight = 'bold'
-}
-
-function saiu(){
-    calcular.style.background = '#033C59'
-    calcular.style.color = 'white'
-}
-
-function tensao(){
-    let corrente = Number(vCorrenteTx.value)
-    let resistencia = Number(vResistenciaTx.value)
-    let potencia = Number(vPotenciaTx.value)
-
-
-    if (corrente != '' && potencia != '' && resistencia != ''){
+    if (corrente > 0 && potencia > 0 && resistencia > 0){
         window.alert('Insira apenas duas grandezas')
+    }else if (corrente <= 0 && potencia <= 0 && resistencia <= 0){
+        alert('Você inseriu três grandezas. Insira apenas duas ')
+    }else if (corrente > 0 && resistencia > 0){
+            res.innerHTML = `Tensão = ${(corrente*resistencia).toFixed(0)}V `
+    }else if (potencia != '' && corrente != ''){
+                res.innerHTML = `Tensão = ${(potencia/corrente).toFixed(0)}V `
     }else{
-        if (corrente != '' && resistencia != ''){
-            vRes.innerHTML = `Tensão = ${corrente*resistencia}V `
-        
-            }else if (potencia != '' && corrente != ''){
-                vRes.innerHTML = `Tensão = ${potencia/corrente}V `
-            }else{
-                vRes.innerHTML = `Tensão = ${potencia**0.5*resistencia**0.5}V `
-            }
+            res.innerHTML = `Tensão = ${(potencia**0.5*resistencia**0.5).toFixed(0)}V `
     }
 }
 
-function corrente(){
-    let tensao = Number(iTensaoTx.value)
-    let resistencia = Number(iResistenciaTx.value)
-    let potencia = Number(iPotenciaTx.value)
 
+function corrente(w , v, o, resultado, calc){
+    let tensao = Number(v.value)
+    let resistencia = Number(o.value)
+    let potencia = Number(w.value)
+    let res = resultado
+    let botao = calc
 
-    if ( tensao != '' && resistencia != '' && potencia != ''){
+    botao.style.background = '#F7EE79'
+    botao.style.color = 'black'
+    botao.style.boxShadow = '1px 1px 3px #64656B'
+    botao.style.fontWeight = 'bold'
+
+    if ( tensao > 0 && potencia > 0 && resistencia > 0){
         window.alert('Você inseriu três grandezas! Insira apenas duas')
-    }else if (tensao == '' && potencia == '' && resistencia == ''){
+    }else if (tensao <= 0 && potencia <= 0 && resistencia <= 0){
         window.alert('Você não inseriu nenhuma grandeza')
-    }else if (tensao == '' && resistencia == '' || tensao == '' && potencia == '' || potencia == '' && resistencia == ''){
-        window.alert('Você inseriu apenas uma grandeza')
+    }else if (tensao <= 0 && potencia <= 0 && resistencia > 0){
+        window.alert('Você inseriu apenas a resistencia elétrica')
+    }else if (tensao > 0 && potencia <= 0 && resistencia <= 0){
+        window.alert('Você inseriu apenas a tensão elétrica')
+    }else if (tensao <= 0 && potencia > 0 && resistencia <= 0){
+        window.alert('Você inseriu apenas a potencia elétrica')
     }else{
-        if (tensao != '' && resistencia != ''){
-            iRes.innerHTML = `Corrente = ${tensao/resistencia} A`
-        }else if (tensao != '' && potencia != ''){
-            iRes.innerHTML = `Corrente = ${potencia/tensao} A`
-        }else if( potencia != '' && resistencia != ''){
-            iRes.innerHTML = `Corrente = ${potencia**0.5/resistencia**0.5} A`
+        if (tensao > 0 && resistencia > 0){
+            res.innerHTML = `Corrente = ${(tensao/resistencia).toFixed(0)} A`
+        }else if (tensao > 0 && potencia > 0){
+            res.innerHTML = `Corrente = ${(potencia/tensao).toFixed(0)} A`
+        }else if( potencia > 0 && resistencia > 0){
+            res.innerHTML = `Corrente = ${(potencia**0.5/resistencia**0.5).toFixed(0)} A`
         }
+    }
+
+} 
+
+function resistencia(w, v, a, resultado, calc){
+    let potencia = Number(w.value)
+    let tensao = Number(v.value)
+    let corrente = Number(a.value)
+    let res = resultado
+    let botao = calc
+
+    botao.style.background = '#F7EE79'
+    botao.style.color = 'black'
+    botao.style.boxShadow = '1px 1px 3px #64656B'
+    botao.style.fontWeight = 'bold'
+    
+
+    if (tensao != '' && corrente != '' && potencia != ''){
+        window.alert('Você inseriu três grandezas, insira apenas duas')
+    }else if (tensao <= 0 && corrente <= 0 && potencia <= 0){
+        window.alert('Você não inseriu nenhuma grandeza')
+    }else if (tensao > 0 && corrente <= 0 && potencia <= 0){
+        alert('Você inseriu apenas o valor da tensão')
+    }else if (tensao <= 0 && corrente > 0 && potencia <= 0){
+        alert('Você inseriu apenas o valor da corrente')
+    }else if (tensao <= 0 && corrente <= 0 && potencia > 0){
+        alert('Você iseriu apenas o valor da potencia')
+
+    }else if (tensao > 0 && corrente > 0){
+        res.innerHTML = `Resistencia = ${(tensao/corrente).toFixed(0)} Ω `
+    }else if( tensao > 0 && potencia > 0 ){
+        res.innerHTML = `Resistencia = ${(tensao**2/potencia).toFixed(0)} Ω`
+    }else if (corrente > 0 && potencia > 0){
+        res.innerHTML = `Resistencia = ${(potencia/corrente**2).toFixed(0)} Ω`
+    }
+
+
+}
+
+function potencia(v, a, o, resultado, calc){
+    let resistencia = Number(o.value)
+    let tensao = Number(v.value)
+    let corrente = Number(a.value)
+    let res = resultado
+    let botao = calc
+
+    botao.style.background = '#F7EE79'
+    botao.style.color = 'black'
+    botao.style.boxShadow = '1px 1px 3px #64656B'
+    botao.style.fontWeight = 'bold'
+    
+
+    if (tensao != '' && corrente != '' && resistencia != ''){
+        window.alert('Você inseriu três grandezas, insira apenas duas')
+    }else if (tensao <= 0 && corrente <= 0 && resistencia <= 0){
+        window.alert('Você não inseriu nenhuma grandeza')
+    }else if (tensao > 0 && corrente <= 0 && resistencia <= 0){
+        alert('Você inseriu apenas o valor da tensão')
+    }else if (tensao <= 0 && corrente > 0 && resistencia <= 0){
+        alert('Você inseriu apenas o valor da corrente')
+    }else if (tensao <= 0 && corrente <= 0 && resistencia > 0){
+        alert('Você iseriu apenas o valor da resistencia')
+
+    }else if (tensao > 0 && corrente > 0){
+        res.innerHTML = `Potencia = ${(tensao*corrente).toFixed(0)} W `
+    }else if( tensao > 0 && resistencia > 0 ){
+        res.innerHTML = `Potencia = ${((tensao**2)/resistencia).toFixed(0)} W`
+    }else if (corrente > 0 && resistencia > 0){
+        res.innerHTML = `Potencia = ${(resistencia*(corrente**2)).toFixed(0)} W`
     }
 
 
