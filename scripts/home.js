b2be536dbc55024d9acd86be782aac878e30ec4a
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-06-05 02:08:43 -0300

    efeito sonoro

--- scripts/home.js
+++ scripts/home.js
@@ -1,27 +1,20 @@
+var somGrandezas = document.getElementById('aud')
+var somResistividade = document.getElementById('aud')
 
-var botao1 = window.document.getElementById('botao-1')
-        botao1.addEventListener('mouseenter', b1_entrar)
-        botao1.addEventListener('mouseout', b1_sair)
 
-        function entrar(){
-            menu.style.textShadow = '0px 0px 10px black'
-            menu.style.color = '#FFD35C'
-        }
+function clicar(button, audio){
+    button.style.scale = '1.2'
+    button.style.border = '4px solid #FFD35C'
+    button.style.boxShadow = '0px 0px 15px white'
+    audio.play()
 
-        function sair(){
-            menu.style.textShadow = 'none'
-            menu.style.color = '#0C1E34'
-        }
+}
 
-        function b1_entrar(){
-            botao1.style.background = 'lightgoldenrodyellow'
-            botao1.style.boxShadow = '0px 0px 30px white'
-            botao1.style.border = '5px solid #FFD35D '
+somGrandezas.addEventListener('ended', function(){
+    window.location.href = 'paginas/grandezas-eletricas.html'
+})
 
-        }
-
-        function b1_sair(){
-            botao1.style.background = 'white'
-            botao1.style.boxShadow ='none'
-            botao1.style.border = '4px solid #FFD35C'
-        }
+somResistividade.addEventListener('ended', function(){
+    window.location.href = 'paginas/resistividade.html'
+})
+        
