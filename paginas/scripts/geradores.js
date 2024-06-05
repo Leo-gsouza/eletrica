920cb3111942c045d190adc8e4e5b5ff6658e6e2
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-06-05 13:26:19 -0300

    termoeletrico pronto

--- paginas/scripts/geradores.js
+++ paginas/scripts/geradores.js
@@ -1,5 +1,10 @@
-function visivel(gerador){
+function visivel(gerador, f2){
     if (gerador.style.display == 'block'){
         gerador.style.display = 'none'
-    }else{gerador.style.display = 'block'}
+
+    }else{gerador.style.display = 'block'
+        f2.style.display = 'none'
+        gerador.style.boxShadow = '0px 0px 15px white'
+    }
+
 }
