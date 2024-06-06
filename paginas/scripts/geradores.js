45fd82e335aec12b9b5a85ccd005b53abbbabfef
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-06-06 13:27:38 -0300

    solar criado

--- paginas/scripts/geradores.js
+++ paginas/scripts/geradores.js
@@ -1,11 +1,13 @@
-function visivel(gerador, f1, f2){
+function visivel(gerador, f1, f2, f3, f4, f5){
     if (gerador.style.display == 'block'){
         gerador.style.display = 'none'
 
     }else{gerador.style.display = 'block'
         f1.style.display = 'none'
         f2.style.display = 'none'
-        gerador.style.boxShadow = '0px 0px 15px white'
+        f3.style.display = 'none'
+        f4.style.display = 'none'
+        f5.style.display = 'none'
     }
 
 }
