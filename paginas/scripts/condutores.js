1f93a3c99a426b1caec75553b8111c0e0334884c
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-06-09 16:28:03 -0300

    queda de tensão sendo criada

--- paginas/scripts/condutores.js
+++ paginas/scripts/condutores.js
@@ -3,9 +3,13 @@ function saibaMais(botao, mais){
     if (mais.style.display == 'block'){
         mais.style.display = 'none'
         botao.style.background = 'lightgoldenrodyellow'
+        botao.style.color = 'black'
+        botao.style.boxShadow = 'none'
         
     }else{mais.style.display = 'block'
-        botao.style.background = '#FFD35C'
+        botao.style.background = '#01537B'
+        botao.style.color = 'white'
+        botao.style.boxShadow = '0px 0px 10px #FFD35C'
     }
 
 }
@@ -17,10 +21,10 @@ function buscar(sec, mat, resultado){
     resultado.style.display = 'block'
     if (material == 'cobre'){
         if (seccao == '0.5'){
-            resultado.innerHTML = `<h1>Condutor de cobre</h1><br> <h2>Secção = ${seccao}mm<sup>2</sup></h2> <br> <p>Capacidade de corrente de 9A com 2 condutores carregados</p> <p>Capacidade de corrente de 8A com 3 condutores carregados</p> `
+            resultado.innerHTML = `<h2>Condutor de cobre<br>Secção = ${seccao}mm<sup>2</sup></h2> <br> <p>Capacidade de corrente de 9A com 2 condutores carregados</p> <p>Capacidade de corrente de 8A com 3 condutores carregados</p> `
 
         }else if(seccao == '0.75'){
-            resultado.innerHTML = `<h1>Condutor de cobre</h1><br> <h2>Secção = ${seccao}mm<sup>2</sup></h2> <br> <p>Capacidade de corrente de 11A com 2 condutores carregados</p> <p>Capacidade de corrente de 10A com 3 condutores carregados</p> `
+            resultado.innerHTML = `<h2>Condutor de cobre<br>Secção = ${seccao}mm<sup>2</sup></h2> <br> <p>Capacidade de corrente de 11A com 2 condutores carregados</p> <p>Capacidade de corrente de 10A com 3 condutores carregados</p> `
         }
 
    }else{resultado.innerHTML = `${seccao} ${material}`}
